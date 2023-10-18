7a4da97616b4e757aecac8e586784a03b704db8d
Andrew Lundy <andrewlundy@icloud.com>
2023-10-17 19:09:27 -0700

    Update some Part 1 queries

--- northwind.sql
+++ northwind.sql
@@ -1,10 +1,11 @@
 -- Start of "Part 1" (https://www.geeksengine.com/database/problem-solving/northwind-queries-part-1.php)
 -- For each order, calculate a subtotal. 
-SELECT order_id, SUM(OrderDetails.Quantity * Products.Price) as Subtotal, COUNT(ProductID) as "Products"
-FROM Orders JOIN OrderDetails USING (OrderID)
-JOIN Products USING (ProductID)
-GROUP BY OrderID
-ORDER BY OrderID;
+SELECT orders.order_id, FORMAT(SUM(order_details.unit_price * order_details.quantity * (1 - discount)), 2) as Subtotal, COUNT(products.product_id) as "Products"
+FROM orders 
+JOIN order_details ON orders.order_id = order_details.order_id
+JOIN products ON order_details.product_id = products.product_id
+GROUP BY orders.order_id
+ORDER BY orders.order_id;
 
 -- Find the total amount of orders for each year.
 SELECT YEAR(orders.order_date) AS Year, FORMAT(SUM(order_details.unit_price * order_details.quantity * (1 - discount)), 2) as Subtotal
@@ -24,29 +25,24 @@ ON orders.order_id = b.order_id
 ORDER BY Year DESC;
 
 -- For each employee, get their total sales amount per country.
-SELECT orders.ship_country, orders.employee_id, FORMAT(SUM(order_details.unit_price * order_details.quantity * (1 - discount)), 2) as Subtotal
+SELECT orders.ship_country, employees.first_name, employees.last_name, FORMAT(SUM(order_details.unit_price * order_details.quantity * (1 - discount)), 2) as Subtotal
 FROM order_details
 JOIN orders ON order_details.order_id = orders.order_id
+JOIN employees ON orders.employee_id = employees.employee_id
 GROUP BY orders.employee_id, orders.ship_country
 ORDER BY orders.employee_id;
 
--- For each employee, get their sales amount, broken down by country name.
-SELECT orders.ship_country, orders.employee_id, FORMAT(SUM(order_details.unit_price * order_details.quantity * (1 - discount)), 2) as Subtotal
-FROM order_details
-JOIN orders ON order_details.order_id = orders.order_id
-GROUP BY orders.employee_id, orders.ship_country
-ORDER BY orders.employee_id;
-
-SELECT orders.ship_country, orders.order_id, orders.employee_id, employees.first_name, employees.last_name, b.Subtotal
+-- For each employee, get their sales details broken down by country.
+SELECT orders.ship_country, orders.order_id, employees.first_name, employees.last_name, b.Subtotal
 FROM orders
-JOIN employees USING (employee_id)
+JOIN employees ON orders.employee_id = employees.employee_id
 JOIN (
 	SELECT order_details.order_id, FORMAT(SUM(order_details.unit_price * order_details.quantity * (1 - discount)), 2) as Subtotal
 	FROM order_details
 	GROUP BY order_details.order_id
 ) as b
 ON orders.order_id = b.order_id
-ORDER BY employees.employee_id;
+ORDER BY employees.first_name, employees.last_name, orders.ship_country;
 
 -- Alphabetical List of Products
 SELECT DISTINCT products.*
