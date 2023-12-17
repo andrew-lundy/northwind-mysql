9b3d21dc35710bf44ebcc3a035b45368f4caa854
Andrew Lundy <andrewlundy@icloud.com>
2023-12-16 18:58:46 -0700

    Optimize query 'find the top salesperson'

--- northwind_queries.sql
+++ northwind_queries.sql
@@ -243,7 +243,7 @@ WHERE row_num = 1;
 
 -- Find the top salesperson for each region; 'top' meaning they have the most sales by total dollar amount.
 -- OPTIMIZATION: If formatting the subtotal is not neccessary on the database layer, `formatted_subtotal` can be removed and the formatting can be done on the application side. In testing, this reduced the mean query duration from 0.0072 to 0.0052.
-SELECT CONCAT(first_name, ' ', last_name) AS salesperson, ship_region AS region
+SELECT CONCAT(first_name, ' ', last_name) AS salesperson, formatted_subtotal, ship_region AS region
 FROM (
 	SELECT 
 		employees.employee_id,
@@ -251,6 +251,7 @@ FROM (
 		employees.last_name,
 		orders.ship_region,
         SUM(order_details.unit_price * order_details.quantity * (1 - discount)) as subtotal,
+        FORMAT(SUM(order_details.unit_price * order_details.quantity * (1 - discount)), 2) as formatted_subtotal,
 		ROW_NUMBER() OVER(PARTITION BY ship_region ORDER BY SUM(order_details.unit_price * order_details.quantity * (1 - discount)) DESC) as row_num
 	FROM employees
 	JOIN orders on employees.employee_id = orders.employee_id
