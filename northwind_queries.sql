00a7b4215c2772fe9d051ac55e0423554b547d4a
Andrew Lundy <andrewlundy@icloud.com>
2024-02-29 21:00:59 -0700

    Update query descriptions

--- northwind_queries.sql
+++ northwind_queries.sql
@@ -264,7 +264,7 @@ SELECT products.product_id, products.product_name, order_details.quantity, order
 FROM order_details
 JOIN orders ON order_details.order_id = orders.order_id
 JOIN products ON order_details.product_id = products.product_id
-WHERE ship_region = 4
+WHERE ship_region = 3
 ORDER BY quantity DESC
 LIMIT 1;
 
